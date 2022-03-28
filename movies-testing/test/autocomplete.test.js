41d2c081bd9eb7912770c608e08b3fca89c84464
DarrelA <darrelaiscoding@gmail.com>
2022-03-28 15:33:49 +0800

    Implement waitFor() for debounce() in movies app & asserting records

--- movies-testing/test/autocomplete.test.js
+++ movies-testing/test/autocomplete.test.js
@@ -1,3 +1,20 @@
+const waitFor = (selector) => {
+  return new Promise((resolve, reject) => {
+    const interval = setInterval(() => {
+      if (document.querySelector(selector)) {
+        clearInterval(interval);
+        clearTimeout(timeout);
+        resolve();
+      }
+    }, 100);
+
+    const timeout = setTimeout(() => {
+      clearInterval(interval);
+      reject();
+    }, 2000);
+  });
+};
+
 // Run createAutoComplete before each test.
 beforeEach(() => {
   document.querySelector('#target').innerHTML = '';
@@ -22,9 +39,25 @@ it('Dropdown start closed', () => {
   expect(dropdown.className).not.to.include('is-active');
 });
 
-it('After searching, dropdown opens up', () => {
+it('After searching, dropdown opens up', async () => {
   const input = document.querySelector('input');
   input.value = 'soft cactus';
   input.dispatchEvent(new Event('input'));
+
+  // waitFor() is implemented for debounce() in autocomplete.js
+  await waitFor('.dropdown-item');
+  const dropdown = document.querySelector('.dropdown');
   expect(dropdown.className).to.include('is-active');
+});
+
+it('After searching, displays some results', async () => {
+  const input = document.querySelector('input');
+  input.value = 'soft cactus';
+  input.dispatchEvent(new Event('input'));
+
+  // waitFor() is implemented for debounce() in autocomplete.js
+  await waitFor('.dropdown-item');
+  const items = document.querySelectorAll('.dropdown-item');
+  expect(items.length).to.equal(3);
+  // expect(items).to.have.lengthOf(3);
 });

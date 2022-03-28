7685f0b0e7cb5dba24fc2502d2beeb53fd40d2b6
DarrelA <darrelaiscoding@gmail.com>
2022-03-28 12:14:25 +0800

    Nodejs assert module

--- index.test.js
+++ index.test.js
@@ -4,6 +4,7 @@
 3. No description on which test went wrong. (FIXED)
 */
 
+const assert = require('assert');
 const { forEach, map } = require('./index');
 
 const test = (desc, fn) => {
@@ -15,22 +16,32 @@ const test = (desc, fn) => {
   }
 };
 
+// https://nodejs.org/dist/latest-v16.x/docs/api/assert.html#assertstrictequalactual-expected-message
+
 test('The forEach function', () => {
   let sum = 0;
   forEach([1, 2, 3], (value) => (sum += value));
-  if (sum !== 6) throw new Error('Expected summing array to equal 6');
+  assert.strictEqual(sum, 6, 'Expected forEach to sum the array');
 });
 
 test('The map function', () => {
   const result = map([1, 2, 3], (value) => value * 2);
 
-  if (result[0] !== 2) {
-    throw new Error(`Expected to find 2, but found ${result[0]}`);
-  }
-  if (result[1] !== 4) {
-    throw new Error(`Expected to find 4, but found ${result[0]}`);
-  }
-  if (result[2] !== 6) {
-    throw new Error(`Expected to find 6, but found ${result[0]}`);
-  }
+  assert.deepStrictEqual(result, [2, 4, 7]);
+
+  assert.strictEqual(
+    result[0],
+    2,
+    `Expected to find 2, but found ${result[0]}`
+  );
+  assert.strictEqual(
+    result[1],
+    4,
+    `Expected to find 4, but found ${result[1]}`
+  );
+  assert.strictEqual(
+    result[2],
+    6,
+    `Expected to find 6, but found ${result[2]}`
+  );
 });
